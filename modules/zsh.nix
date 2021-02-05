c8fddd639ad67f17c14ccfdfb4b6f9b676b9b260
abnopanda <abnopanda@gmail.com>
2021-02-04 19:23:37 -0800

    add asdf to zsh + define emac keymaps

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -5,6 +5,16 @@ let
 in
 {
   programs.zsh = {
+    initExtraBeforeCompInit = ''
+      ENABLE_ASDF=true
+      # asdf
+      if [[ $ENABLE_ASDF && -f "$HOME/.asdf/asdf.sh" ]]; then
+        echo "ASDF: Enabled"
+        . "$HOME/.asdf/asdf.sh"
+      fi
+      # append completions to fpath
+      fpath=($ASDF_DIR/completions $fpath)
+    '';
     initExtra = ''
 #        # for single user mode
 #        DIR=~/.nix-profile/etc/profile.d
@@ -12,13 +22,8 @@ in
 #        [[ -f "$DIR/hm-session-vars.sh" ]] && . "$DIR/hm-session-vars.sh"
         eval $(dircolors ~/.nix-profile/share/LS_COLORS)
     '';
-#    prezto = {
-#      enable = true;
-#      prompt = {
-#        theme = "steeef";
-#      };
-#      utility.safeOps = false;
-#    };
+
+    defaultKeymap = "emacs";
   };
 
 }
